a38888924a6097a15c5e0421ef8212263723e828
ryosyntaxx <riodwi12174@gmail.com>
2024-01-19 19:31:57 +0700

    update class logs

--- src/utils/logs.py
+++ src/utils/logs.py
@@ -1,8 +1,51 @@
+from zlib import crc32
+from time import strftime
+
 class Logs:
 
-
-    def success():
+    def succes(self, 
+                  status: str, 
+                  total: int, 
+                  failed: int, 
+                  success: int,
+                  source: str
+                  ) -> None:
+        
+        content = {
+              "Crawlling_time": strftime('%Y-%m-%d %H:%M:%S'),
+              "id_project": crc32('gofood'.encode('utf-8')),
+              "id": crc32(source.encode('utf-8')),
+              "project":"gofood",
+              "source_name": source,
+              "total_data": total,
+              "total_success": success,
+              "total_failed": failed,
+              "status": status,
+              "assign": self.PIC
+            }
+        
+        with open(self.LOG_PATH_SUC, 'a+', encoding= "utf-8") as file:
+            file.write(f'{str(content)}\n')
         ...
 
-    def error():
-        ...
+    def error(self,
+                  status: str, 
+                  source: str,
+                  message: str
+                  ) -> None:
+        
+        content =   {
+                "Crawlling_time": strftime('%Y-%m-%d %H:%M:%S'),
+                "id_project": crc32('gofood'.encode('utf-8')),
+                "project":"gofood",
+                "source_name": source,
+                "id": crc32(source.encode('utf-8')),
+                "process_name": "Crawling",
+                "status": "error",
+                "type_error": status,
+                "detail_error": message,
+                "assign": self.PIC
+            }
+
+        with open(self.LOG_PATH_ERR, 'a+', encoding= "utf-8") as file:
+            file.write(f'{str(content)}\n')
